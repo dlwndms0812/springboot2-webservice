cf34f791aa41568fb8ad61c03afc690a2b1c9b5a
dlwndms0812 <dlwndms0812@naver.com>
2022-01-17 17:20:27 +0900

    junit->junit5

--- src/test/java/com/jueun/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jueun/springboot/web/HelloControllerTest.java
@@ -1,53 +1,58 @@
 package com.jueun.springboot.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.jueun.springboot.config.auth.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@ExtendWith(SpringExtension.class) // junit5
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
-    public void hello가_리턴된다() throws Exception{
-        String hello="hello";
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
 
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
-    public void helloDto가_리턴된다() throws Exception{
-        String name="hello";
-        int amount=1000;
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
 
         mvc.perform(
-                get("/hello/dto")
-                .param("name",name)
-                .param("amount",String.valueOf(amount)))
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
-
     }
 }
-
-//@RunWith(SpringRunner.class)
+//@ExtendWith(SpringExtension.class)
 //1. 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행
-//2. 여기서는 SpringRunner라는 실행자 사용
+//2. 여기서는 SpringExtension라는 실행자 사용
 //3. 즉, 스프링 부트 테스트와 JUnit 사이에 연결자 역할
 
 //WebMvcTest
